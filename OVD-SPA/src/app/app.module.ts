83ffdf2e6f41f49cf10a1a3569b16bba1ac819b8
Random Murican <RandomMurican@gmail.com>
2019-02-28 16:21:34 -0600

    Some nav functionality

--- OVD-SPA/src/app/app.module.ts
+++ OVD-SPA/src/app/app.module.ts
@@ -2,15 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './Users/ej/School/OVD/OVD-SPA/src/nav/nav.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      NavComponent,
+      NavComponent
+   ],
+   imports: [
+      BrowserModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
---
+++ OVD-SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  // Grab input
+  model: any = {};
+
+  // Nothing to do on init
+  ngOnInit() {}
+
+  // If the username is name then fake a login
+  login() {
+    if (this.model.username === 'name') {
+      console.log('Congrats');
+    } else {
+      console.log('nope');
+    }
+  }
+
+  // Lets just hardcode where we are
+  loggedIn() {
+    return true;
+  }
+
+  // Lets hardcode whether we're admin
+  isAdmin() {
+    return true;
+  }
+}
