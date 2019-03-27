b7ec87131adc0af7a7b682bbfb0ff066a49fd7af
Mark <mrbeussink@gmail.com>
2019-03-27 16:30:47 -0500

    Added login form to home component

--- OVD-SPA/src/app/home/home.component.ts
+++ OVD-SPA/src/app/home/home.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  model: any = {};
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  login() {
+    
   }
 
 }
