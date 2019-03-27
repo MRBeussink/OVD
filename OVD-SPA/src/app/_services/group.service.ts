cc68cacc66268080409de8a6570249cb735f72a4
Random Murican <RandomMurican@gmail.com>
2019-03-26 19:16:32 -0500

    Communication stuff

--- OVD-SPA/src/app/_services/group.service.ts
+++ OVD-SPA/src/app/_services/group.service.ts
@@ -2,19 +2,24 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AuthService } from '../_services/auth.service';
 import { environment } from '../../environments/environment';
+import { GroupsComponent } from '../groups/groups.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GroupService {
   baseUrl = environment.apiURL + 'realadmin' + '/NewGroup/';
+  activeGroup = '';
+  editType = '';
   groups = new Map();
+  images = [];
 
   constructor(private http: HttpClient, public authService: AuthService) {
     // These are hard coded samples, remove these for the final product
     this.groups.set('Name', [3, 8, 2]);
     this.groups.set('Sample', [5, 7, 3]);
     this.groups.set('CS499 Test Group', [250, 300, 50]);
+    this.images = ['Ubuntu 1.6', 'Windows 10 U1.2', 'Debian 4.8', 'MS DOS v4'];
   }
 
 create(model: any) {
@@ -23,8 +28,20 @@ create(model: any) {
   if (this.groups.has(model.group_name)) {
     return false;
   }
-  this.groups.set(model.group_name, [0, 10, 3]);
+  const running = Math.floor(Math.random() * model.total_vm);
+  const active = Math.floor(Math.random() * running);
+
+  this.groups.set(model.group_name, [active, running, model.total_vm - running]);
   return true;
+}
+
+update(oldName: String, model: any) {
+  if (this.groups.has(oldName) && !this.groups.has(model.group_name)) {
+    this.delete(oldName);
+    this.create(model);
+    return true;
+  }
+  return false;
 }
 
 delete(group: String) {
@@ -36,6 +53,14 @@ delete(group: String) {
 getGroups() {
   console.log(this.http.get(this.baseUrl));
   return this.groups;
+}
+
+getGroup(name: String) {
+  console.log(this.groups.get(name));
+}
+
+getImages() {
+  return this.images;
 }
 
 }
