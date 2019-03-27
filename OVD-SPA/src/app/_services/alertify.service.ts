bf645e13b264fab5f7cf6d05a48c560ec06d8ae2
Random Murican <RandomMurican@gmail.com>
2019-03-27 16:49:15 -0500

    Closer to modal form

--- OVD-SPA/src/app/_services/alertify.service.ts
+++ OVD-SPA/src/app/_services/alertify.service.ts
@@ -18,13 +18,32 @@ confirm(message: string, okCallback: () => any) {
 }
 
 newGroup() {
-  alertify.prompt(
-    'New Group',
-    'Group Name', 'name'
-  , function(evt, value) {
-    console.log(name);
-  }
-  , function() { });
+  alertify.genericDialog('genericDialog', function() {
+    return {
+        main: function(content) {
+            this.setContent(content);
+        },
+        setup: function() {
+            return {
+                focus: {
+                    element: function() {
+                        return this.elements.body.querySelector(this.get('selector'));
+                    },
+                    select: true
+                },
+                options: {
+                    basic: true,
+                    maximizable: false,
+                    resizable: false,
+                    padding: false
+                }
+            };
+        },
+        settings: {
+            selector: undefined
+        }
+    };
+});
 }
 
 }
