207cbdfe6d88680742a5cfec01628f962561a280
Random Murican <RandomMurican@gmail.com>
2019-03-27 17:04:55 -0500

    Login works again

--- OVD-SPA/src/app/home/home.component.ts
+++ OVD-SPA/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -9,13 +11,32 @@ export class HomeComponent implements OnInit {
 
   model: any = {};
 
-  constructor() { }
+  constructor(private router: Router, public authService: AuthService) { }
 
   ngOnInit() {
   }
 
   login() {
-    
+    this.authService.login(this.model).subscribe(next => { // What to do when successful
+
+    }, error => { // What to do on error
+
+    }, () => { // What to do after success
+      // Admins go to dashboard, users to user page
+      if (this.isAdmin()) {
+        this.router.navigate(['/dashboard']);
+      } else {
+        this.router.navigate(['']);
+      }
+    });
+
+    // Clear login form
+    this.model.username = '';
+    this.model.password = '';
+  }
+
+  isAdmin() {
+    return this.authService.isAdmin();
   }
 
 }
